00790e7651bc3a81b486b5288ec6bc205ac4c669
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-07-27 21:17:20 +0100

    need to add dynamics to video

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "./styles/partials/_resets.scss";
 import './App.scss';
 import Header from './component/Header/Header';
 import Video from './component/Video/Video';
+import {useState} from "react"
+import videosJson from "./assets/Data/videos.json"
+import videoDetailsJson from "./assets/Data/video-details.json"
+
 
 function App() {
+  const [videos, setVidoes] = useState(videosJson);
+  const [videoDetails, setVideoDetails] = useState(videoDetailsJson);
+  const [currentVideo, setCurrentVideo] = useState(videoDetailsJson[0]);
+
   return (
     <div className="App">
-      
       <Header />
-      <Video />
+      <Video currentVideo={currentVideo} videoDetails = {videoDetails} />
+      {/* setCurrentVideo(videoObj)  */}
     </div>
   );
 }
--- src/component/video/Video.js
+++ src/component/video/Video.js
@@ -1,27 +1,31 @@
-import video from "../../assets/Data/videos.json";
 import { useState } from "react";
+import "./Video.scss";
 
-function Video() {
-  // const [video,setVideo] = useState([])
+function Video({ currentVideo }) {
+
+  
   return (
     <section className="video">
-      <video className="video__main" poster={video.image} controls>
-        {/* adding the source tag */}
-        {/* <source src={video.video}/> */}
-        
-      </video>
+      <video
+        className="video__mainVid"
+        poster={currentVideo.image}
+        controls
+      ></video>
+      <section className="video__overview">
+        {/* i got the first video title */}
+        <h1 className="video__title">{currentVideo.title}</h1>
 
-      <section>
-        {/*  i got the first video title */}
-        <h1 className="Video__title">{video[0].title}</h1>
-        {/* {video.map(video =>{
-            return(
-                <h1>{video.title}</h1>
-            )
-        })} */}
+        <div className="video__analytics">
+          <p>{currentVideo.channel}</p>
+          <p>{currentVideo.views}</p>
+          <p>{currentVideo.timestamp}</p>
+          <p>{currentVideo.likes}</p>
+        </div>
 
+        <div className="video__description">
+          <p>{currentVideo.description}</p>
+        </div>
       </section>
-
     </section>
   );
 }
