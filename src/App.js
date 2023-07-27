f8e41c6e6f634b95f51a648e78f3425da23137e0
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-07-27 06:19:58 +0100

    doing the video/picture

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles/partials/_resets.scss";
 
 import './App.scss';
 import Header from './component/Header/Header';
+import Video from './component/Video/Video';
 
 function App() {
   return (
     <div className="App">
       
       <Header />
+      <Video />
     </div>
   );
 }
--- src/component/video/Video.js
+++ src/component/video/Video.js
@@ -1,8 +1,30 @@
-import video from "../../assets/Data/video-details.json"
+import video from "../../assets/Data/videos.json";
+import { useState } from "react";
 
-// function Video(){
-//     return(
+function Video() {
+  // const [video,setVideo] = useState([])
+  return (
+    <section className="video">
+      <video className="video__main" poster={video.image} controls>
+        {/* adding the source tag */}
+        {/* <source src={video.video}/> */}
         
+      </video>
 
-//     )
-// }
+      <section>
+        {/*  i got the first video title */}
+        <h1 className="Video__title">{video[0].title}</h1>
+        {/* {video.map(video =>{
+            return(
+                <h1>{video.title}</h1>
+            )
+        })} */}
+
+      </section>
+
+    </section>
+  );
+}
+
+// exporting the file
+export default Video;
