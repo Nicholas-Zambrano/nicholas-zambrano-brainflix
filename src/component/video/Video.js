44eb6711b6d93757ede82d943922ce423bd29135
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-07-30 07:49:13 +0100

    styling the comments for tablet section

--- src/component/video/Video.js
+++ src/component/video/Video.js
@@ -23,18 +23,35 @@ function Video({ currentVideo }) {
 
           <div className="video__analytics">
             <div>
-            <h3 className="video__channel">{currentVideo.channel}</h3>
-            
+              <h3 className="video__channel">{currentVideo.channel}</h3>
             </div>
-            <div className="video__viewsWrapper">
+
+            {/* <section className="video__viewsDateWrapper">
+            diplicated it here */}
+            <div className="video__viewsWrapper ">
               <div className="video__viewsImg"></div>
               <p className="video__views  video__analytics--pTags">
                 {currentVideo.views}
               </p>
             </div>
-            <p className="video__date video__analytics--pTags">
+
+            <section className="video__viewsDateWrapperTablet">
+              <div className="video__viewsWrapperTablet">
+                <div className="video__viewsImg"></div>
+                <p className="video__viewsTablet  video__analytics--pTags">
+                  {currentVideo.views}
+                </p>
+              </div>
+              <p className="video__dateTablet video__analytics--pTagsTablet">
+                {convertedDate}
+              </p>
+            </section>
+
+            {/* duplicated here */}
+            <p className="video__date video__analytics--pTags  video__dateTablet">
               {convertedDate}
             </p>
+            {/* </section> */}
             {/* <div> */}
             <div className="video__likesWrapper">
               <div className="video__likesImg"></div>
